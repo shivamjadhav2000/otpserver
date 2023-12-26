c70ab6825eb24aec46bd9538a7c39f3b9d1beb5d
shivamjadhav <shivamjadhav348@gmail.com>
2023-12-25 23:32:33 -0600

    completed otp server

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-
+require('dotenv').config();
+const msg91AuthKey = process.env.Msg91AuthKey;
+const TemplateId = process.env.TemplateId;
 const app = express();
 const PORT = 3001;
-
 app.use(express.json());
 app.use(cors())
 app.post('/sendOtp', async (req, res) => {
   const {  mobile } = req.body;
-  console.log("mobiile",mobile)
-
   const options = {
     method: 'POST',
-    url: `https://control.msg91.com/api/v5/otp?template_id=63249ae3d6fc05295321dce5&mobile=${mobile}&otp_length=6`,
+    url: `https://control.msg91.com/api/v5/otp?template_id${TemplateId}=&mobile=${mobile}&otp_length=6`,
     headers: {
       'accept': 'application/json',
       'content-type': 'application/json',
-      'authkey': '382603AbwKCJey63234fbbP1'
+      'authkey': msg91AuthKey
     },
   };
 
@@ -46,13 +45,12 @@ app.post('/sendOtp', async (req, res) => {
 app.get('/verifyOtp', async (req, res) => {
     const otp = req.query.otp;
     const mobile = req.query.mobile;
-    console.log("req.query",req.query)
     const options = {
       method: 'GET',
       url: `https://control.msg91.com/api/v5/otp/verify?otp=${otp}&mobile=${mobile}`,
       headers: {
         'accept': 'application/json',
-        'authkey': '382603AbwKCJey63234fbbP1'
+        'authkey': msg91AuthKey
       }
     };
   
